e38513a307877bc6ec16403f541807a633097d44
Madiha001 <madiha.bcss19@iba-suk.edu.pk>
2022-02-28 15:05:52 +0500

    Updated user and auth files and package.json

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,15 +1,43 @@
 const router = require("express").Router();
 const User = require("../schemas/User");
+const bcrypt = require("bcrypt");
 
-router.get("/register", async (req,res) => {
-    const user = await new User({
-        username: "madi",
-        email: "madi@gmail.com",
-        password: "09876543"
-        
-    })
-    await user.save();
-    res.send("For testing purpose");
+//Register functionality
+router.post("/register", async (req,res) => {
+
+    try{
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+        const NewUser = await new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPassword
+            
+            })
+
+        const user = await NewUser.save();
+        res.status(200).json(user);
+    }
+    catch(err){
+        res.status(500).json(err)
+    }
+})
+
+//Login functionality
+router.post("/login", async (req,res) => {
+    try{
+        const user = await User.findOne({email: req.body.email});
+        !user && res.status(404).json("user not found");
+
+        const validPassword = await bcrypt.compare(req.body.password, user.password)
+        !validPassword && res.status(400).json("wrong password")
+
+        res.status(200).json(user)
+    }
+    catch(err){
+        res.status(500).json(err)
+    }
 })
 
 module.exports = router
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,32 @@
 const router = require("express").Router();
+const User = require("../schemas/User");
+const bcrypt = require("bcrypt");
 
-router.get("/", (req,res) => {
-    res.send("Welcome to user Route!!!")
-})
+//update user
+router.put("/:id", async (req, res) => {
+    if (req.body.userId === req.params.id || req.body.isAdmin) {
+        if (req.body.password) {
+          try {
+            const salt = await bcrypt.genSalt(10);
+            req.body.password = await bcrypt.hash(req.body.password, salt);
+          } 
+          catch (err) {
+            return res.status(500).json(err);
+          }
+        }
+        try {
+          const user = await User.findByIdAndUpdate(req.params.id, {
+            $set: req.body,
+          });
+          res.status(200).json("User account has been updated");
+        } 
+        catch (err) {
+          return res.status(500).json(err);
+        }
+      } 
+      else {
+        return res.status(403).json("You can not update other account if you are not an admin!");
+      }
+    });    
 
 module.exports = router
